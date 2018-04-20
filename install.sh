9277a4db94d07ff0148ff94d4e34ce4133c7f11f
0xFA15E5EC <nicholas@evalagency.com>
2018-04-20 11:53:45 -0400

    seed vals

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-#!/system/bin/sh
-# create database
+#!/usr/bin/env bash
+# seed database
 sqlite3 .db.sqlite3 "CREATE TABLE hs (id INTEGER PRIMARY KEY NOT NULL, lat VARCHAR(12), lon VARCHAR(12), bssid VARCHAR(17) UNIQUE, essid VARCHAR(255), psk VARCHAR(64), prcsd INT(1) DEFAULT NULL)"
