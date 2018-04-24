4f5a16faa8c6dcc9e62dc7e60ee5ae05fa12edde
tehw0lf <tehwolf@protonmail.com>
2018-04-24 20:36:23 +0200

    Fix shortcuts
    For Android devices, the shortcuts need to run `sh script.sh` for execution, while on Linux `./script.sh` can be used. The created scripts should be executable directly.

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ fi
 # determine shebang
 if [ "$device" -eq 0 ]; then
     sh_loc=`which sh`
-    echo "creating scripts. run them using sh script.sh"
+    echo "creating scripts. run them using sh script.sh or move the created shortcuts to an executable location."
 elif [ "$device" -eq 1 ]; then
     if [ -e "/usr/bin/env" ]; then
         sh_loc="/usr/bin/env bash"
@@ -27,16 +27,28 @@ fi
 # create files
 echo "#!$sh_loc" > "$instpath/"airba.sh
 cat "$instpath/src/airbash" >> "$instpath/airba.sh"
+chmod +x "$instpath/"airba.sh
 echo "#!$sh_loc" > "$instpath/"crackdefault.sh
 cat "$instpath/src/crackdefault" >> "$instpath/crackdefault.sh"
+chmod +x "$instpath/"crackdefault.sh
 
 # create shortcuts that can be moved to a folder that is on $PATH
 echo "#!$sh_loc" > "$instpath/"airbash
 echo "cd $instpath" >> "$instpath/"airbash
-echo "./airba.sh $@" >> "$instpath/"airbash
+if [ "$device" -eq 0 ]; then
+    echo "sh airba.sh $@" >> "$instpath/"airbash
+else
+    echo "./airba.sh $@" >> "$instpath/"airbash
+fi
+chmod +x "$instpath/"airbash
 echo "#!$sh_loc" > "$instpath/"crackdefault
 echo "cd $instpath" >> "$instpath/"crackdefault
-echo "./crackdefault.sh $@" >> "$instpath/"crackdefault
+if [ "$device" -eq 0 ]; then
+    echo "sh crackdefault.sh $@" >> "$instpath/"crackdefault
+else
+    echo "./crackdefault.sh $@" >> "$instpath/"crackdefault
+fi
+chmod +x "$instpath/"crackdefault
 
 # create static files
 mkdir "$instpath/.loc" # &>/dev/null
