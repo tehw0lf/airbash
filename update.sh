cdfdaf75a65a3369fb1645761e35d70ea4695883
tehw0lf <tehwolf@protonmail.com>
2018-04-26 18:47:07 +0200

    Add updater for Linux
    A working update script for Linux is now available.

---
+++ update.sh
@@ -0,0 +1,14 @@
+# installation path
+instpath=`pwd`
+
+# get platform - there is no git on android
+if [ ! -d "/system" ]; then
+    tmp="/tmp/airbash"
+    
+    # download latest version
+    rm -rf "$tmp/"
+    git clone https://github.com/tehw0lf/airbash "$tmp/"
+    cp -R "$tmp/." "$instpath/" 2>/dev/null     # suppress git overwrite errors
+    echo "installing update... INTERFACE will be reset to wlan0"
+    sh "$instpath/"install.sh
+fi
